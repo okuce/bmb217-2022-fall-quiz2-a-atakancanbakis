cdb545ce0af1a2e494b4c6ece20d653aa4af459c
atakancanbakis <114661827+atakancanbakis@users.noreply.github.com>
2022-10-06 14:29:04 +0300

    Update Otobus.py

--- Otobus.py
+++ Otobus.py
@@ -1,18 +1,58 @@
+"""
+Adi:Atakan
+Soyadi:Canbakış
+Numarasi:20217170006
+Bolumu:Bilgisayar Mühendisliği 2.sinif
+
+"""
+
 class Otobus:
     """Otobus bilet satis takip sinifi"""
+    plaka:str=""
+    nereden:str=""
+    nereye:str=""
+    koltukno:int=0
+    dolukoltuk:int=0
+    boskoltuk:int=0
+    koltuksay:int=0
     
 
-    def bilet_sat(self):
+    def __init__(self,plaka,nereden,nereye,koltuksay):
         """Otobusteki dolu koltuk sayisini 1 artirir"""
+        self.plaka=plaka
+        self.nereden=nereden
+        self.nereye=nereye
+        self.koltuksay=koltuksay
         
-    
+    def bilet_sat(self,):
+        """Otobusteki dolu koltuk sayisini 1 artirir"""
+        if self.koltukno==0:
+            self.dolukoltuk=self.dolukoltuk
+        else:
+            self.dolukoltuk=+1
+            
+
+        
+   
     def bilet_iade(self):
         """Otobusteki dolu koltuk sayisini 1 azaltir"""
+        if self.koltukno==0:
+            self.boskoltuk=self.boskoltuk
+        else:
+            self.boskoltuk=-1
+        
         
 
     
     def durum_yaz(self):
         """Otobusun guzergahini, plakasini,bos ve dolu koltuk sayisini yazdirir"""
+        print("{},{},{},{},{},".format(self.nereden,self.nereye,self.plaka,self.boskoltuk,self.dolukoltuk))
+        
+
+
+        
+
+
         
 
 
